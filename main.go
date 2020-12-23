08e6ac4ea8fef881d33409f9602fa5af1e30e0bd
辻気流 <kiryu.18011106@gmail.com>
2020-12-23 21:43:08 +0900

    test:日付変換

--- main.go
+++ main.go
@@ -2,14 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"strconv"
+	"strings"
+	"time"
 )
 
+func sliceAtoi(sa []string) []int {
+	si := make([]int, 0, len(sa))
+	for i := 0; i < len(sa); i++ {
+		nu, _ := strconv.Atoi(sa[i])
+		si = append(si, nu)
+	}
+	return si
+}
 func main() {
-	in := [...]float64{10, 6, 3}
-	// l := in[0]
-	// a := in[1]
-	// b := in[2]
-	log.Printf("%#v", in)
-	fmt.Printf("%#v", in)
+
+	in := [...]string{"2010 12 30 0 0", "2018 12 30 0 0"}
+	// in_slice := [...][...]string{}
+	inslice := make([][]int, 2)
+	temp := make([]string, 5)
+	for i := 0; i < len(in); i++ {
+		temp = strings.Split(in[i], " ")
+		inslice[i] = sliceAtoi(temp)
+	}
+	// fmt.Printf("%#v\n", inslice)
+	timeSlice := make([]time.Time, 2)
+	for i := 0; i < len(inslice); i++ {
+		timeSlice[i] = time.Date(inslice[i][0], time.Month(inslice[i][1]), inslice[i][2], inslice[i][3], inslice[i][4], 0, 0, time.UTC)
+	}
+	// fmt.Printf("%#v\n", timeSlice)
+	duration := timeSlice[1].Sub(timeSlice[0])
+	hours0 := int(duration.Hours())
+	year := hours0 / (24 * 365)
+	month := (hours0 % 24) / 12
+	days := ((hours0 % (24 * 365)) % 12) / 24
+	hours := hours0 % 24
+	mins := int(duration.Minutes()) % 60
+	fmt.Println(year, month, days, hours, mins)
+	fmt.Println((hours0 / 24) % 365)
 }
